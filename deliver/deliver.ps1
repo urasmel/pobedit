22ca6b1ca11a67ebd605cd5a25ef7afda2468ef1
urasmel <smelykh.yura@mail.ru>
2024-09-26 12:26:31 +0300

    P-18 Update deliver file

--- deliver/deliver.ps1
+++ deliver/deliver.ps1
@@ -1,2 +1,6 @@
+Write-Output "Current location is:"
+Get-Location
+Write-Output "Starting build process..."
 Start-Process npm -ArgumentList "run", "build" -wait
+Write-Output "Starting run process..."
 Start-Process npm -ArgumentList "run", "dev"
