0ee1d6bd77c1418cc334af926264c0202be94d50
urasmel <smelykh.yura@mail.ru>
2024-09-27 11:57:21 +0300

    P-18 Update jenkins file

--- deliver/deliver.ps1
+++ deliver/deliver.ps1
@@ -1,6 +1,8 @@
 Write-Output "Current location is:"
 $PWD.Path
 Write-Output "Starting build process..."
-Start-Process npm -ArgumentList "run", "build" -wait
+# Start-Process npm -ArgumentList "run", "build" -wait
+npm run build
 Write-Output "Starting run process..."
-Start-Process npm -ArgumentList "run", "dev"
+# Start-Process npm -ArgumentList "run", "dev"
+npm run dev
