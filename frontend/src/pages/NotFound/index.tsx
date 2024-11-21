2d6d63671c9c2a06d3a19b818453d6594f8d72f8
urasmel <smelykh.yura@mail.ru>
2024-11-21 18:16:47 +0300

    P-30 Create not Found page

--- frontend/src/pages/NotFound/index.tsx
+++ frontend/src/pages/NotFound/index.tsx
@@ -1,9 +1,15 @@
+import { Button } from '@mui/material';
+import styles from './styles.module.css';
+import { NavLink } from 'react-router-dom';
 
 const NotFound = () => {
 
     return (
-        <div >
-            NOT FOUND
+        <div className={styles.not_found}>
+            Страница не найдена
+            <Button component={NavLink} to="/" variant="contained" color="primary">
+                На главную
+            </Button>
         </div>
     );
 };
