4d56eba61ad512aaea7523d30daacd2272fadacf
urasmel <smelykh.yura@mail.ru>
2024-11-21 12:27:11 +0300

    P-18 Fix readme, channelMainInfo component

--- frontend/src/components/common/ChannelMainInfo/index.tsx
+++ frontend/src/components/common/ChannelMainInfo/index.tsx
@@ -3,10 +3,11 @@ import styles from './styles.module.css';
 
 export const ChannelMainInfo = (props: ChannelMainInfoProps) => {
     return (
-        <div>
-            <div className={styles["widget"]}>
-                <div className={styles["widget__message"]}>Заголовок канала: {props.title}</div>
-                <div className={styles["widget__message"]}>Иденификатор канала: {props.id}</div>
-            </div></div>
+        <div className={styles.widget}>
+            <div className={styles.widget__message}>Канал: {props.title}</div>
+            <div className={styles.widget__message}>Иденификатор канала: {props.id}
+
+            </div>
+        </div>
     );
 };
