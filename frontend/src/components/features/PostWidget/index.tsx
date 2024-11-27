3e2547c2d49417589a980914ec74eeb59e26d1da
urasmel <smelykh.yura@mail.ru>
2024-11-27 17:29:42 +0300

    P-40 Markup comments section of postwidget

--- frontend/src/components/features/PostWidget/index.tsx
+++ frontend/src/components/features/PostWidget/index.tsx
@@ -1,17 +1,35 @@
 import styles from "./styles.module.css";
 import type { Post } from "types/Post";
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { Link } from "@mui/material";
 
 const PostWidget = (post: Post) => {
     return (
-        <div className={styles.widget}>
-            <div className={styles.widget__header}>
+        <div className={styles.post}>
+            <div className={styles.post__header}>
                 Ид. сообщения: {post.postId}, время:{" "}
                 {new Date(post.date).toLocaleString("ru-RU")}
             </div>
-            <div className={styles["widget__message-container"]}>
-                <pre className={styles.widget__message}>{post.message}</pre>
+            <div className={styles["post__message-container"]}>
+                <pre className={styles.post__message}>{post.message}</pre>
             </div>
-        </div>
+            {
+                post.comments.length === 0
+                    ?
+                    <div className={styles.post__comments}>
+                        <div className={styles["post__comments-count"]}>
+                            {post.comments.length} комментариев
+                        </div>
+                        <div className={styles["post__comments-link"]}>
+                            <Link target="_blank" href={`/posts/${post.postId}/channels/${post.peerId}/comments`}>
+                                <ChevronRightIcon />
+                            </Link>
+                        </div>
+                    </div>
+                    :
+                    <></>
+            }
+        </div >
     );
 };
 
