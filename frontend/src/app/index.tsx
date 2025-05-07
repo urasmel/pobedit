efd891be5e22a592c2582c5e3e69f6350c98d052
urasmel <smelykh.yura@mail.ru>
2025-05-07 17:13:38 +0300

    P-4 Fix css problem

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -2,14 +2,13 @@ import { Aside } from '../widgets/Aside';
 import { Footer } from "../widgets/Footer/Footer";
 import { Header } from "../widgets/Header/Header";
 import { AppRouter } from "./routers";
-import styles from "./styles/index.module.css";
 import { queryClient } from "@/shared/api/query-client";
 import { Providers } from './providers';
-
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './styles/index.scss';
 import { theme, ThemeContext } from './theme';
+import { Box } from '@mui/material';
 
 
 const rootNode = document.getElementById('root') as Element;
@@ -17,26 +16,41 @@ const root = createRoot(rootNode);
 root.render(
     // <React.StrictMode>
     <BrowserRouter>
-        <div className={styles.app}>
+        <Box sx={{
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "100vh",
+            boxSizing: "border-box",
+            padding: ".5rem"
+        }}>
 
             <Header />
 
-            <div className={styles.main_block}>
+            <Box sx={{
+                display: "flex",
+                gap: "1rem",
+                flex: "3 1 1vw",
+                marginTop: "1rem"
+            }}>
 
                 <Aside />
 
-                <div className={styles.main_content}>
+                <Box sx={{
+                    flex: "6 6 85vw",
+                    boxShadow: "var(--shadow)",
+                    borderRadius: ".3rem"
+                }}>
                     <ThemeContext.Provider value={theme}>
                         <Providers client={queryClient} >
                             <AppRouter />
                         </Providers>
                     </ThemeContext.Provider>
-                </div>
+                </Box>
 
-            </div>
+            </Box>
 
             <Footer />
-        </div>
+        </Box>
     </BrowserRouter >
     // </React.StrictMode>
 );
